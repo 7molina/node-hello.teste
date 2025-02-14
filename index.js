ad0696ded08eef205061c988fda7dcc7449bba82
MOLINA <theepic2007@gmail.com>
2025-02-14 12:22:29 -0300

    second commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Tonelli vacilao\n'
+  const msg = 'pedrinho vacilao\n'
   res.end(msg);
 });
 
